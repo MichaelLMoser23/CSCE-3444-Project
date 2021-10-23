8700b5cc2b090a7bcd095b182ab9c5bfe01679e7
hnguyen <hnguyen1276@gmail.com>
2021-10-23 02:26:26 -0500

    Added post and comment functionality
    Also removed background on homepage for now

--- project/squawk/home/urls.py
+++ project/squawk/home/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from home import views
 
 urlpatterns = [
-    path('home/', views.Index.as_view(), name='index'),
+    path('', views.Index.as_view(), name='index'),
     path('home/login', views.Login.as_view(), name='login'),
     path('home/registration', views.Registration.as_view(), name='registration'),
     path('logout_user', views.logout_user, name='logout')
